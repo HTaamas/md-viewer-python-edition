f8c490da972840c42281d26045d810d7114ce162
HTaamas <96864185+HTaamas@users.noreply.github.com>
2024-07-21 00:38:06 +0200

    Start

---
+++ app.py
@@ -0,0 +1,50 @@
+import requests
+from flask import Flask, render_template, abort, send_from_directory
+import os
+import markdown
+
+app = Flask(__name__)
+app.config['MD_FOLDER'] = 'md_files'
+
+def get_file_content(filename):
+    try:
+        with open(os.path.join(app.config['MD_FOLDER'], filename), 'r', encoding='utf-8') as file:
+            return file.read()
+    except IOError:
+        return None
+
+@app.route('/')
+def index():
+    files = os.listdir(app.config['MD_FOLDER'])
+    return render_template('index.html', files=files)
+
+@app.route('/view/<filename>')
+def view_file(filename):
+    if not filename.endswith(('.md')):
+        abort(404)
+    if filename.endswith('.mdx'):
+        view_mdx_file(filename)
+    else:
+        content = get_file_content(filename)
+        if content is None:
+            abort(404)
+        rendered_content = markdown.markdown(content)
+    
+    return render_template('view.html', content=rendered_content, filename=filename)
+
+@app.route('/view_mdx/<filename>')
+def view_mdx_file(filename):
+    if not filename.endswith('.mdx'):
+        abort(404)
+    else:
+        content = get_file_content(filename)
+        if content is None:
+            abort(404)
+    return render_template('mdx_view.html', content=content, filename=filename)
+
+@app.route('/md_files/<path:filename>')
+def download_file(filename):
+    return send_from_directory(app.config['MD_FOLDER'], filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
